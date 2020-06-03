a5484a614bbd8b3dc1f49e282dcf0927ce823b75
Sam-Chen <x20911x@gmail.com>
2020-06-03 10:37:53 +0800

    ch1 p21 Split Loop

--- statement_p7.js
+++ statement_p7.js
@@ -29,18 +29,19 @@ plays = {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice[0].customer}\n`;
-    // const format = new Intl.NumberFormat("en-US",
-    //                       { style: "currency", currency: "USD",
-    //                         minimumFractionDigits: 2 }).format;
+
   
     for (let perf of invoice[0].performances) {
 
-      volumeCredits += volumeCreditsFor(perf);
-  
       // print line for this order
       result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
+    for (let perf of invoice[0].performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+
+
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
