eeb28ecdd6b483c93dcd358294182680c3350d00
Sam-Chen <x20911x@gmail.com>
2020-06-03 10:53:24 +0800

    ch1 p23

--- statement_p7.js
+++ statement_p7.js
@@ -42,7 +42,7 @@ plays = {
     let volumeCredits = totalVolumeCredits();
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
 
