83e022b16eab0f6a4cb35986846fb2755968fe32
Sam-Chen <x20911x@gmail.com>
2020-06-03 15:40:56 +0800

    ch1 p35

--- statement_p7.js
+++ statement_p7.js
@@ -1,3 +1,5 @@
+
+
 invoice = [
   {
     "customer": "BigCo",
@@ -24,92 +26,91 @@ plays = {
 }  
 
 
-
 function statement (invoice, plays){
   return renderPlainText(createStatementData (invoice, plays));
 }
 
-function createStatementData (invoice, plays){
-  const statementData = {};
-  statementData.customer = invoice[0].customer
-  statementData.performances = invoice[0].performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+// function createStatementData (invoice, plays){
+//   const statementData = {};
+//   statementData.customer = invoice[0].customer
+//   statementData.performances = invoice[0].performances.map(enrichPerformance);
+//   statementData.totalAmount = totalAmount(statementData);
+//   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+//   return statementData;
 
 
-  // function totalAmount(data){
-  //   let result = 0;
-  //   for (let perf of data.performances) {
-  //     result += perf.amount;
-  //   }
-  //   return result
-  // }
+//   // function totalAmount(data){
+//   //   let result = 0;
+//   //   for (let perf of data.performances) {
+//   //     result += perf.amount;
+//   //   }
+//   //   return result
+//   // }
 
-  function totalAmount(data) {
-    return data.performances
-      .reduce((total, p) => total + p.amount, 0);
-  }
-
-
-  // function totalVolumeCredits(data){
-  //   let result = 0;
-  //   for (let perf of data.performances) {
-  //     result += perf.volumeCredits;
-  //   }
-  //   return result;
-  // }
-  function totalVolumeCredits(data) {
-    return data.performances
-      .reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result)
-    return result;
-  }
-
-  function playFor(aPerformance){
-    return plays[aPerformance.playID]
-  }
+//   function totalAmount(data) {
+//     return data.performances
+//       .reduce((total, p) => total + p.amount, 0);
+//   }
 
 
-  function amountFor(aPerformance){
-    let result = 0;
-    switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result
-  }
+//   // function totalVolumeCredits(data){
+//   //   let result = 0;
+//   //   for (let perf of data.performances) {
+//   //     result += perf.volumeCredits;
+//   //   }
+//   //   return result;
+//   // }
+//   function totalVolumeCredits(data) {
+//     return data.performances
+//       .reduce((total, p) => total + p.volumeCredits, 0);
+//   }
+
+//   function enrichPerformance(aPerformance) {
+//     const result = Object.assign({}, aPerformance);
+//     result.play = playFor(result);
+//     result.amount = amountFor(result);
+//     result.volumeCredits = volumeCreditsFor(result)
+//     return result;
+//   }
+
+//   function playFor(aPerformance){
+//     return plays[aPerformance.playID]
+//   }
 
 
-  function volumeCreditsFor(aPerformance){
-    let result = 0;
-    // add volume credits
-    result += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result
-  }
+//   function amountFor(aPerformance){
+//     let result = 0;
+//     switch (aPerformance.play.type) {
+//     case "tragedy":
+//       result = 40000;
+//       if (aPerformance.audience > 30) {
+//         result += 1000 * (aPerformance.audience - 30);
+//       }
+//       break;
+//     case "comedy":
+//       result = 30000;
+//       if (aPerformance.audience > 20) {
+//         result += 10000 + 500 * (aPerformance.audience - 20);
+//       }
+//       result += 300 * aPerformance.audience;
+//       break;
+//     default:
+//         throw new Error(`unknown type: ${aPerformance.play.type}`);
+//     }
+//     return result
+//   }
+
+
+//   function volumeCreditsFor(aPerformance){
+//     let result = 0;
+//     // add volume credits
+//     result += Math.max(aPerformance.audience - 30, 0);
+//     // add extra credit for every ten comedy attendees
+//     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+//     return result
+//   }
   
-}
+// }
 
 
 
@@ -125,6 +126,13 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+
+
+  function usd(aNumber){
+    return new Intl.NumberFormat("en-US",
+    { style: "currency", currency: "USD",
+      minimumFractionDigits: 2 }).format(aNumber/100)
+  }
 
   // function amountFor(aPerformance){
   //   let result = 0;
@@ -163,14 +171,6 @@ function renderPlainText(data) {
   //   return result
   // }
 
-
-  function usd(aNumber){
-    return new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD",
-      minimumFractionDigits: 2 }).format(aNumber/100)
-  }
-
-
   // function totalVolumeCredits(){
   //   let result = 0;
   //   for (let perf of data.performances) {
@@ -179,10 +179,7 @@ function renderPlainText(data) {
   //   return result;
   // }
 
-  function totalAmount(data) {
-    return data.performances
-      .reduce((total, p) => total + p.amount, 0);
-  }
+ 
   // function totalAmount(){
   //   let result = 0;
   //   for (let perf of data.performances) {
