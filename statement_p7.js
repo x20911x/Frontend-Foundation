48bfa016a7c0ba28acbe82177e510afe1c473b2f
Sam-Chen <x20911x@gmail.com>
2020-06-03 09:04:28 +0800

    ch1 p20 Change Function Declaration

--- statement_p7.js
+++ statement_p7.js
@@ -38,10 +38,10 @@ plays = {
       volumeCredits += volumeCreditsFor(perf);
   
       // print line for this order
-      result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+      result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
@@ -89,10 +89,10 @@ plays = {
       return result
     }
 
-    function format(aNumber){
+    function usd(aNumber){
       return new Intl.NumberFormat("en-US",
       { style: "currency", currency: "USD",
-        minimumFractionDigits: 2 }).format(aNumber)
+        minimumFractionDigits: 2 }).format(aNumber/100)
     }
   }
 
