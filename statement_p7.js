8e6ca69d8415a698730a412ccb92a260dd2e3855
Sam-Chen <x20911x@gmail.com>
2020-06-03 16:45:53 +0800

    ch1 p36 extract two js file

--- statement_p7.js
+++ statement_p7.js
@@ -1,30 +1,5 @@
 
-
-invoice = [
-  {
-    "customer": "BigCo",
-    "performances": [
-      {
-        "playID": "hamlet",
-        "audience": 55
-      },
-      {
-        "playID": "as-like",
-        "audience": 35
-      },
-      {
-        "playID": "othello",
-        "audience": 40
-      }
-    ]
-  }
-]
-plays = {
-  "hamlet": {"name": "Hamlet", "type": "tragedy"},
-  "as-like": {"name": "As You Like It", "type": "comedy"},
-  "othello": {"name": "Othello", "type": "tragedy"}
-}  
-
+// import createStatementData from './createStatementData.js';
 
 function statement (invoice, plays){
   return renderPlainText(createStatementData (invoice, plays));
@@ -127,13 +102,6 @@ function renderPlainText(data) {
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 
-
-  function usd(aNumber){
-    return new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD",
-      minimumFractionDigits: 2 }).format(aNumber/100)
-  }
-
   // function amountFor(aPerformance){
   //   let result = 0;
   //   switch (aPerformance.play.type) {
@@ -187,16 +155,46 @@ function renderPlainText(data) {
   //   }
   //   return result
   // }
-
-
   }
 
+  
+function htmlStatement (invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+function renderHtml (data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+
+
+
+
+
+
+
+
+function usd(aNumber){
+  return new Intl.NumberFormat("en-US",
+  { style: "currency", currency: "USD",
+    minimumFractionDigits: 2 }).format(aNumber/100)
+}
 
 
 
 // console.log(invoice[0].aPerformanceormances)
 // console.log(plays)
 console.log(statement(invoice, plays))
+document.write(htmlStatement(invoice, plays))
 
 
 
